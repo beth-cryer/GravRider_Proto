982d757503141e101517b9fcdbfe1a8b0a7a53c9
Beth Cryer <52399621+beth-cryer@users.noreply.github.com>
2025-01-28 02:39:54 +0000

    Sticks to ground surfaces

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float gravForce = 10.0f;
     [SerializeField] private Vector3 gravDirection = Vector3.down;
     [SerializeField] private float rotateSpeed = 180.0f;
+    [SerializeField] private float groundAlignSpeed = 45.0f;
 
     [SerializeField] private Transform hoverModel;
 
@@ -49,43 +50,73 @@ public class PlayerController : MonoBehaviour
                 gravDirection = Vector3.down;
             }
 
-            targetRotation = Quaternion.LookRotation(transform.forward, -gravDirection);
-            //targetRotation = Quaternion.FromToRotation(transform.up, -gravDirection);
+            SwitchGrav();
+
+            //Rotate hitbox instantly to correct direction
+            transform.rotation = targetRotation;
         }
 
         //Rotate model slowly to correct direction
         hoverModel.rotation = Quaternion.RotateTowards(hoverModel.rotation, targetRotation, rotateSpeed * Time.deltaTime);
 
-        //Rotate hitbox instantly to correct direction
-        transform.rotation = targetRotation;
         transform.Rotate(-gravDirection, x * turnSpeed * Time.deltaTime, Space.World);
 
         Vector3 forwardVector = transform.forward * moveSpeed;
         transform.position = transform.position + (forwardVector * Time.deltaTime);
+
+
+        //Angle to slope
+        RaycastHit hit;
+        var grounded = Physics.Raycast(transform.position, -transform.up, out hit, 2f, LayerMask.GetMask("Ground"));
+
+        var slopeRotation = Quaternion.FromToRotation(transform.up, hit.normal);
+        Quaternion forward = Quaternion.LookRotation(transform.forward, transform.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, slopeRotation * forward, groundAlignSpeed * Time.deltaTime);
     }
 
     private void FixedUpdate()
     {
-
         Vector3 gravVector = gravDirection * gravForce;
+        rb.AddForce(gravVector, ForceMode.Acceleration);
+    }
 
-        rb.AddForce(gravVector, ForceMode.Force);
+    private void SwitchGrav()
+    {
+        targetRotation = Quaternion.LookRotation(transform.forward, -gravDirection);
+
+        //targetRotation = Quaternion.FromToRotation(transform.up, -gravDirection);
+
+        //rb.AddForce(-gravSurfaceNear.up * 10.0f, ForceMode.Impulse);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        /*
-        Vector3 normal = collision.GetContact(0).normal;
-        //Vector3 slopeForward = Vector3.ProjectOnPlane(transform.forward, normal).normalized;
+        Quaternion rot = Quaternion.FromToRotation(transform.up, collision.transform.up);
 
-        Quaternion rot = Quaternion.FromToRotation(transform.up, normal);
-        //Quaternion rot = Quaternion.LookRotation(transform.forward, normal);
+        //transform.rotation = rot;
+    }
 
-        transform.rotation = rot;
-        */
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform.tag.Equals("GravitySurface"))
+        {
+            gravDirection = Vector3.down;
+            SwitchGrav();
+
+            //Rotate hitbox instantly to correct direction
+            transform.rotation = targetRotation;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("GravitySurface"))
+        {
+            gravSurfaceNear = other.transform;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
     {
         if (other.tag.Equals("GravitySurface"))
         {
